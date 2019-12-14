391573e3be1c20accffc95eff9be8004262d8398
Manu Bhardwaj <mnbhardwaj888@gmail.com>
2019-12-14 07:41:17 +0530

    [add] mongo setup

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,11 +1,23 @@
 import logger from "../util/logger.util";
-import {APP_IDENTIFIER} from "../util/secrets.util";
+import {APP_IDENTIFIER, DB_PASS, DB_PATH, DB_USER} from "../util/secrets.util";
+import mongoose from "mongoose";
+import Sample from "../models/sample.model";
 
 class DBService {
   private static _instance: DBService;
 
   private constructor() {
     logger.silly(`[${APP_IDENTIFIER}] DBService`);
+
+    const options = {
+      useNewUrlParser: true,
+      user: DB_USER,
+      pass: DB_PASS,
+    };
+
+    mongoose.connect(DB_PATH, options)
+      .then(() => logger.silly("Connected to MongoDb..."))
+      .catch((error) => logger.error(error));
 
     this.initModels();
   }
@@ -14,7 +26,9 @@ class DBService {
     return this._instance || (this._instance = new DBService());
   }
 
-  private initModels(): void {}
+  private initModels(): void {
+    Sample
+  }
 }
 
 export const dbService = DBService.getInstance();
