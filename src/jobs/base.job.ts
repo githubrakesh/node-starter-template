094d27d210a3cf2451f2d52b416e7f11ff845be4
Manu Bhardwaj <mnbhardwaj888@gmail.com>
2020-01-25 03:00:14 +0530

    [update] cron schedule setup

--- src/jobs/base.job.ts
+++ src/jobs/base.job.ts
@@ -1,15 +1,24 @@
 import {schedule} from "node-cron";
 import {Scheduler} from "./scheduler";
+import fs from "fs";
 
 export abstract class BaseJob extends Scheduler {
   schedule(): BaseJob {
-    const className = this.constructor.name.split("Job")[0];
-    const file = require(`${__dirname}/${className}.job`);
-    const key = Object.keys(file)[0];
-    const job: BaseJob = file[key];
+    const job: BaseJob = this.getJob();
     schedule(this.cronExpression, job.handle);
     return this;
   }
 
   abstract async handle(): Promise<unknown>;
+
+  private getJob(): BaseJob {
+    const fileName = fs.readdirSync(__dirname).find(file => {
+      return this.constructor.name.toLowerCase().includes(
+        file.toLowerCase().split(".")[0]
+      );
+    });
+    const file = require(`${__dirname}/${fileName}`);
+    const key = Object.keys(file)[0];
+    return file[key];
+  }
 }
